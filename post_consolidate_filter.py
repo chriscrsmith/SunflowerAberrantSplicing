c6b48297a48a8d2a6debd832e934ed2595e37c3c
Chris C Smith <chris@cu-genvpn-tcom-10.180.163.192.int.colorado.edu>
2021-06-03 09:53:40 -0700

    script for filtering after the isoform consolidation step

---
+++ post_consolidate_filter.py
@@ -0,0 +1,52 @@
+
+# quick filter to remove genes with no splicing after consolidating alleles from same isoform 
+# example: python post_consolidate_filter.py list_of_important_isoforms.txt good_isos.txt consolidatedIsos.txt > list_of_important_isoforms_consolidated.txt
+
+
+import sys
+
+### read in list of isoforms to look at
+my_isos = []
+with open(sys.argv[1]) as infile:
+    for line in infile:
+        iso = line.strip().split()[0]
+        my_isos.append(iso)
+
+
+
+### read in the "good_isos" list
+good_isos = {}
+with open(sys.argv[2]) as infile:
+    for line in infile:
+        new_gene, isos = line.strip().split()
+        isos = isos.split(",")
+        for iso in isos:
+            if iso in my_isos:
+                old_gene = new_gene.split(".")[0]
+                if old_gene not in good_isos: # some are repeated                                                                                                                          
+                    good_isos[old_gene] = {}
+                good_isos[old_gene][new_gene] = [] # adding in the new gene                                                                                                                
+                for iso in isos:
+                    good_isos[old_gene][new_gene].append(iso)
+
+                
+
+### read in consolidated isoforms
+with open(sys.argv[3]) as infile:
+    for line in infile:
+        newline = line.strip().split()
+        iso = newline[0]
+        old_gene = "_".join(iso.split("_")[0:4])
+        clusters = newline[1].split("|")
+        keep = True # default
+        for new_gene in good_isos[old_gene]:
+            if iso in good_isos[old_gene][new_gene]:
+                for cluster in clusters:
+                    if len(cluster.split(",")) == len(good_isos[old_gene][new_gene]):
+                        keep = False
+                if keep == True:
+                        print (line.strip())
+                else:
+                    sys.stderr.write("\tfiltered " + "_".join(new_gene.split("_")[0:4]) + "\n")
+
+           
